103c264509161ebdf02492f4478c1f9fd99c736b
David Case <david.case@shruggr.cloud>
2023-06-27 01:14:26 -0400

    Rollback rabbitmq

--- rabbitmq/main.go
+++ rabbitmq/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"database/sql"
-	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"log"
@@ -13,7 +12,6 @@ import (
 	"github.com/bitcoinsv/bsvd/wire"
 	"github.com/joho/godotenv"
 	"github.com/libsv/go-bt/v2"
-	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/redis/go-redis/v9"
 	"github.com/shruggr/1sat-indexer/indexer"
 	"github.com/shruggr/1sat-indexer/lib"
@@ -21,7 +19,7 @@ import (
 
 const INDEXER = "node"
 
-// var THREADS uint64 = 16
+var THREADS uint64 = 16
 
 var db *sql.DB
 
@@ -38,19 +36,11 @@ var settle = make(chan *indexer.BlockCtx, 1000)
 
 var rdb *redis.Client
 
-var ch *amqp.Channel
-var txnQ amqp.Queue
-var doneQ amqp.Queue
-
-// var ctx context.Context
-
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Panicf("%s: %s", msg, err)
 	}
 }
-
-var currentBlockId string
 
 func init() {
 	godotenv.Load("../.env")
@@ -89,91 +79,10 @@ func init() {
 	// 		log.Panic(err)
 	// 	}
 	// }
-
-	rmq, err := amqp.Dial(os.Getenv("RABBITMQ"))
-	failOnError(err, "Failed to connect to RabbitMQ")
-
-	ch, err = rmq.Channel()
-	failOnError(err, "Failed to open a channel")
-
-	doneQ, err = ch.QueueDeclare(
-		"",    // name
-		false, // durable
-		true,  // delete when unused
-		true,  // exclusive
-		false, // no-wait
-		nil,   // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	// err = ch.ExchangeDeclare(
-	// 	"blocks", // name
-	// 	"fanout", // type
-	// 	true,     // durable
-	// 	false,    // auto-deleted
-	// 	false,    // internal
-	// 	false,    // no-wait
-	// 	nil,      // arguments
-	// )
-	// failOnError(err, "Failed to declare an exchange")
-
-	txnQ, err = ch.QueueDeclare(
-		"txns", // name
-		false,  // durable
-		false,  // delete when unused
-		false,  // exclusive
-		false,  // no-wait
-		amqp.Table{"x-consumer-timeout": 10000},
-	)
-	failOnError(err, "Failed to declare a queue")
 }
 
 func main() {
-	msgs, err := ch.Consume(
-		doneQ.Name, // queue
-		"",         // consumer
-		false,      // auto-ack
-		false,      // exclusive
-		false,      // no-local
-		false,      // no-wait
-		nil,        // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	go func() {
-		for d := range msgs {
-			// fmt.Printf("Received a message: %x\n", d.Body)
-			hexid := d.CorrelationId
-
-			var txn *lib.Txn
-			var ok bool
-			if txn, ok = indexer.Txns[hexid]; !ok {
-				continue
-			}
-			orphans := make([]*lib.Txn, 0)
-			indexer.M.Lock()
-			delete(indexer.Txns, txn.HexId)
-			for _, child := range txn.Children {
-				delete(child.Parents, txn.HexId)
-				orphan := len(child.Parents) == 0
-				if orphan {
-					orphans = append(orphans, child)
-				}
-			}
-			indexer.Ctx.Fees += binary.BigEndian.Uint64(d.Body)
-			indexer.M.Unlock()
-			for _, orphan := range orphans {
-				indexer.InQueue++
-				indexer.Wg.Add(1)
-				enqueuTxn(orphan)
-			}
-			indexer.InQueue--
-			indexer.Wg.Done()
-			fmt.Printf("Indexed: %d %d %s %d %d %v\n", txn.Height, txn.Idx, txn.HexId, len(indexer.TxnQueue), len(indexer.Txns), indexer.InQueue)
-		}
-	}()
-
-	// go indexer.ProcessTxns(uint(THREADS))
+	go indexer.ProcessTxns(uint(THREADS))
 	go processCompletions()
 
 	var blockId string
@@ -202,23 +111,20 @@ func main() {
 
 func processBlock(height uint32, blockId string) (err error) {
 	fmt.Println("Processing Block", height)
-
-	// err = ch.PublishWithContext(context.Background(),
-	// 	"blocks", // exchange
-	// 	"",       // routing key
-	// 	false,    // mandatory
-	// 	false,
-	// 	amqp.Publishing{
-	// 		ContentType: "text/plain",
-	// 		Body:        []byte(blockId),
-	// 	},
-	// )
-	// failOnError(err, "Failed to publish a message")
-
 	r, err := os.OpenFile(fmt.Sprintf("/home/shruggr/blocks/%s.bin", blockId), os.O_RDONLY, 0644)
 	failOnError(err, "Failed to open block file")
 	defer r.Close()
 
+	// block, err := bit.GetBlockByHeight(int(height))
+	// if err != nil {
+	// 	log.Panicln(height, err)
+	// }
+
+	// // fmt.Printf("Block %s\n", block.Hash)
+	// r, err := bit.GetRawBlockRest(block.Hash)
+	// if err != nil {
+	// 	log.Panicln(height, err)
+	// }
 	protocolVersion := wire.ProtocolVersion
 	wireBlockHeader := wire.BlockHeader{}
 	err = wireBlockHeader.Bsvdecode(r, protocolVersion, wire.BaseEncoding)
@@ -242,6 +148,7 @@ func processBlock(height uint32, blockId string) (err error) {
 	}
 
 	for idx = 0; idx < int(txCount); idx++ {
+
 		txn := &lib.Txn{
 			Tx:       bt.NewTx(),
 			Height:   height,
@@ -272,38 +179,14 @@ func processBlock(height uint32, blockId string) (err error) {
 		if len(txn.Parents) == 0 {
 			indexer.InQueue++
 			indexer.Wg.Add(1)
-			enqueuTxn(txn)
+			indexer.TxnQueue <- txn
 		}
 	}
 
 	indexer.Wg.Wait()
 	// rdb.Publish(context.Background(), "indexed", fmt.Sprintf("%d", indexer.Ctx.Height))
-
 	settle <- indexer.Ctx
 	return nil
-}
-
-func enqueuTxn(txn *lib.Txn) {
-	body := txn.Bytes()
-	// log.Printf("Sent a message: %v", txn)
-	// var body bytes.Buffer
-	// enc := gob.NewEncoder(&body)
-	// err := enc.Encode(task)
-	// failOnError(err, "Failed to encode txn")
-
-	err := ch.PublishWithContext(context.Background(),
-		"",        // exchange
-		txnQ.Name, // routing key
-		false,     // mandatory
-		false,
-		amqp.Publishing{
-			ContentType:   "application/octet-stream",
-			ReplyTo:       doneQ.Name,
-			CorrelationId: txn.HexId,
-			Body:          body,
-		})
-	failOnError(err, "Failed to publish a message")
-	// log.Printf(" [x] Sent %s", clone.HexId)
 }
 
 func processCompletions() {
