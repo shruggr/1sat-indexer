0f8bc2af4155f681a154ca09b1647f1a3fea6da6
David Case <david.case@shruggr.cloud>
2024-08-14 18:23:41 +0000

    update spends index

--- migrations/8_sales.up.sql
+++ migrations/8_sales.up.sql
@@ -10,8 +10,7 @@ WHERE spend != '\x';
 ALTER TABLE bsv20_txos ADD COLUMN IF NOT EXISTS sale BOOLEAN;
 ALTER TABLE bsv20_txos ADD COLUMN IF NOT EXISTS spend_height INTEGER;
 ALTER TABLE bsv20_txos ADD COLUMN IF NOT EXISTS spend_idx BIGINT;
-CREATE INDEX IF NOT EXISTS idx_bsv20_txos_spend ON bsv20_txos(spend)
-WHERE spend != '\x';
+CREATE INDEX IF NOT EXISTS idx_bsv20_txos_spend_all ON bsv20_txos(spend);
 CREATE INDEX IF NOT EXISTS idx_bsv20_txos_sales ON bsv20_txos(sale, spend_height, spend_idx)
 WHERE spend != '\x';
 CREATE INDEX IF NOT EXISTS idx_bsv20_txos_tick_sales ON bsv20_txos(tick, spend_height, spend_idx)
