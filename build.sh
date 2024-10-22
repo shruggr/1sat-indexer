596a38ffc7434dbbea900b5e38882c64a22d3836
David Case <david.case@shruggr.cloud>
2024-10-21 21:30:04 -0500

    update build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 go build cmd/block-sync/block-sync.go
 go build cmd/ingest/ingest.go
+go build cmd/origin/origin.go
+go build cmd/owner-sync/owner-sync.go
 go build cmd/server/server.go
 go build cmd/subscribe/subscribe.go
-go build cmd/owner-sync/owner-sync.go
-go build cmd/origin/origin.go
