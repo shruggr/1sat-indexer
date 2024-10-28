1f68bd71d5b6867a6b11f119d2e23a0fb98fdbf3
David Case <david.case@shruggr.cloud>
2024-10-28 22:27:49 +0000

    bsv21

--- cmd/bsv21/bsv21.go
+++ cmd/bsv21/bsv21.go
@@ -11,15 +11,19 @@ import (
 	"github.com/shruggr/1sat-indexer/evt"
 	"github.com/shruggr/1sat-indexer/idx"
 	"github.com/shruggr/1sat-indexer/onesat"
+	"github.com/shruggr/1sat-indexer/sub"
 )
 
+var TAG = "bsv21"
 var PAGE_SIZE = uint32(1000)
-
 var CONCURRENCY uint
+var TOPIC string
+var VERBOSE uint
+
 var ctx = context.Background()
 
 var ingest = &idx.IngestCtx{
-	Tag: "bsv21",
+	Tag: TAG,
 	Indexers: []idx.Indexer{
 		&onesat.InscriptionIndexer{},
 		&onesat.Bsv21Indexer{},
@@ -31,11 +35,17 @@ var ingest = &idx.IngestCtx{
 }
 
 func main() {
+	flag.StringVar(&TOPIC, "t", "", "Junglebus SubscriptionID")
 	flag.UintVar(&CONCURRENCY, "c", 1, "Concurrency")
+	flag.UintVar(&VERBOSE, "v", 0, "Verbose")
 	flag.Parse()
 
 	limiter := make(chan struct{}, CONCURRENCY)
 
+	if TOPIC != "" {
+		go subscribe()
+	}
+	go categorize()
 	if tokenIds, err := idx.SearchOutpoints(ctx, &idx.SearchCfg{
 		Key: evt.EventKey(onesat.BSV21_TAG, &evt.Event{
 			Id:    "issue",
@@ -73,22 +83,20 @@ func processToken(tokenId string) (err error) {
 		return err
 	} else {
 		bsv21 := bsv21Data.Data.(*onesat.Bsv21)
-		if bsv21.FundAddress == "" {
-			idx.AcctDB.ZAddNX(ctx, idx.OwnerSyncKey, redis.Z{
-				Score:  0,
-				Member: bsv21.FundAddress,
-			})
-		}
-		pendingKey := evt.EventKey(onesat.BSV21_TAG, &evt.Event{
-			Id:    onesat.PendingEvent,
-			Value: bsv21.Id,
+		idx.AcctDB.ZAddNX(ctx, idx.OwnerSyncKey, redis.Z{
+			Score:  0,
+			Member: bsv21.FundAddress,
 		})
-		if pendingCount, err := idx.TxoDB.ZCard(ctx, pendingKey).Result(); err != nil {
-			log.Println("Error counting pending", bsv21.FundAddress, err)
-			return err
-		} else if pendingCount == 0 {
-			return nil
-		}
+		// pendingKey := evt.EventKey(onesat.BSV21_TAG, &evt.Event{
+		// 	Id:    onesat.PendingEvent,
+		// 	Value: bsv21.Id,
+		// })
+		// if pendingCount, err := idx.TxoDB.ZCard(ctx, pendingKey).Result(); err != nil {
+		// 	log.Println("Error counting pending", bsv21.FundAddress, err)
+		// 	return err
+		// } else if pendingCount == 0 {
+		// 	return nil
+		// }
 
 		if balance, err := idx.Balance(ctx, idx.OwnerTxosKey(bsv21.FundAddress)); err != nil {
 			log.Println("Error getting balance", tokenId, bsv21.FundAddress, err)
@@ -106,38 +114,137 @@ func processToken(tokenId string) (err error) {
 			log.Println("Error counting invalid", tokenId, bsv21.FundAddress, err)
 			return err
 		} else {
-			validationCount := validCount + invalidCount
-			validationCost := validationCount * onesat.BSV21_INDEX_FEE
-			if balance < validationCost {
-				log.Println("Insufficient balance", tokenId, bsv21.FundAddress, balance, validationCost)
-				return nil
-			}
-			limit := uint32((balance - uint64(validationCost)) / onesat.BSV21_INDEX_FEE)
-			if limit == 0 {
-				log.Println("Insufficient balance", tokenId, bsv21.FundAddress, balance, validationCost)
-				return nil
-			}
-			tokenIngest := *ingest
-			tokenIngest.Limit = limit
-			onIngest := func(ctx context.Context, idxCtx *idx.IndexContext) error {
-				for _, txo := range idxCtx.Txos {
-					if idxData, ok := txo.Data[onesat.BSV21_TAG]; ok {
-						if bsv21, ok := idxData.Data.(*onesat.Bsv21); ok {
-							if bsv21.Id != tokenId || bsv21.Status == onesat.Pending {
-								continue
-							}
-							idx.TxoDB.ZRem(ctx, pendingKey, txo.Outpoint.String())
-						}
+			validationCount := int64(validCount + invalidCount)
+			for balance = balance - (validationCount * onesat.BSV21_INDEX_FEE); balance > 0; {
+				if items, err := idx.QueueDB.ZRange(ctx, idx.QueueKey(tokenId), 0, 0).Result(); err != nil {
+					log.Println("Error getting queue", tokenId, err)
+					return err
+				} else if len(items) == 0 {
+					break
+				} else {
+					txid := items[0]
+					if idxCtx, err := ingest.ParseTxid(ctx, txid, idx.AncestorConfig{
+						Load:  true,
+						Parse: true,
+					}); err != nil {
+						log.Println("Error parsing txid", txid, err)
+						return err
+					} else {
+						// for _, txo := range idxCtx.Txos {
+						// 	if idxData, ok := txo.Data[onesat.BSV21_TAG]; ok {
+						// 		if bsv21, ok := idxData.Data.(*onesat.Bsv21); ok {
+						// 			if bsv21.Id != tokenId || bsv21.Status == onesat.Pending {
+						// 				continue
+						// 			}
+						// 			idx.TxoDB.ZRem(ctx, idx.QueueKey(tokenId), txo.Outpoint.String())
+						// 		}
+						// 	}
+						// }
+						// if err := txo.Save(ctx, idxCtx.Height, idxCtx.Idx); err != nil {
+						// 	log.Println("Error saving txo", txid, err)
+						// 	return err
+						// }
 					}
 				}
-				return nil
 			}
-			tokenIngest.OnIngest = &onIngest
-			if err := tokenIngest.Exec(ctx); err != nil {
-				log.Println("Error ingesting token", tokenId, err)
-				return err
-			}
+			// if   {
+			// 	log.Println("Insufficient balance", tokenId, bsv21.FundAddress, balance, validationCost)
+			// 	return nil
+			// }
+
+			// limit := uint32((balance - uint64(validationCost)) / onesat.BSV21_INDEX_FEE)
+			// if limit == 0 {
+			// 	log.Println("Insufficient balance", tokenId, bsv21.FundAddress, balance, validationCost)
+			// 	return nil
+			// }
+			// tokenIngest := *ingest
+			// tokenIngest.Limit = limit
+			// onIngest := func(ctx context.Context, idxCtx *idx.IndexContext) error {
+			// 	for _, txo := range idxCtx.Txos {
+			// 		if idxData, ok := txo.Data[onesat.BSV21_TAG]; ok {
+			// 			if bsv21, ok := idxData.Data.(*onesat.Bsv21); ok {
+			// 				if bsv21.Id != tokenId || bsv21.Status == onesat.Pending {
+			// 					continue
+			// 				}
+			// 				idx.TxoDB.ZRem(ctx, pendingKey, txo.Outpoint.String())
+			// 			}
+			// 		}
+			// 	}
+			// 	return nil
+			// }
+			// tokenIngest.OnIngest = &onIngest
+			// if err := tokenIngest.Exec(ctx); err != nil {
+			// 	log.Println("Error ingesting token", tokenId, err)
+			// 	return err
+			// }
 		}
 	}
 	return nil
+}
+
+var queueKey = idx.QueueKey(TAG)
+
+func subscribe() {
+	if err := (&sub.Sub{
+		Tag:          TAG,
+		Queue:        TAG,
+		Topic:        TOPIC,
+		FromBlock:    801000,
+		IndexBlocks:  true,
+		IndexMempool: true,
+		Verbose:      VERBOSE > 0,
+	}).Exec(ctx); err != nil {
+		log.Panic(err)
+	}
+}
+
+func categorize() {
+	if txids, err := idx.QueueDB.ZRangeArgs(ctx, redis.ZRangeArgs{
+		Key:   queueKey,
+		Start: 0,
+		Stop:  -1,
+	}).Result(); err != nil {
+		log.Panic(err)
+	} else {
+		limiter := make(chan struct{}, CONCURRENCY)
+		// errors := make(chan error)
+		for _, txid := range txids {
+			go func(txid string) {
+				defer func() {
+					<-limiter
+					// done <- txid
+				}()
+				if idxCtx, err := ingest.ParseTxid(ctx, txid, idx.AncestorConfig{
+					Load:  true,
+					Parse: true,
+				}); err != nil {
+					panic(err)
+				} else {
+					for _, txo := range idxCtx.Txos {
+						if bsv21Data, ok := txo.Data[onesat.BSV21_TAG]; ok {
+							bsv21 := bsv21Data.Data.(*onesat.Bsv21)
+							if bsv21.Op == "deploy+mint" {
+								if err = txo.Save(ctx, idxCtx.Height, idxCtx.Idx); err != nil {
+									panic(err)
+								}
+							} else {
+								idx.AcctDB.ZAddNX(ctx, idx.QueueKey(bsv21.Id), redis.Z{
+									Score:  0,
+									Member: txo.Outpoint.TxidHex(),
+								})
+							}
+						}
+					}
+
+					if err = idx.QueueDB.ZRem(ctx, queueKey, txid).Err(); err != nil {
+						log.Panic(err)
+					}
+				}
+			}(txid)
+		}
+		if len(txids) == 0 {
+			// log.Println("No transactions to ingest")
+			time.Sleep(time.Second)
+		}
+	}
 }
