b23a9e856dd57d6b0869e825c8f74e1adeaeec75
David Case <david.case@shruggr.cloud>
2025-03-13 05:25:02 +0000

    ancestor config

--- cmd/ingest/ingest.go
+++ cmd/ingest/ingest.go
@@ -13,12 +13,16 @@ var CONCURRENCY uint
 var VERBOSE int
 var QUEUE string
 var TAG string
+var ancestorConfig idx.AncestorConfig
 
 func init() {
 	flag.StringVar(&TAG, "tag", idx.IngestTag, "Log tag")
 	flag.StringVar(&QUEUE, "q", idx.IngestTag, "Queue tag")
 	flag.UintVar(&CONCURRENCY, "c", 1, "Concurrency")
 	flag.IntVar(&VERBOSE, "v", 0, "Verbose")
+	flag.BoolVar(&ancestorConfig.Load, "l", false, "Load ancestors")
+	flag.BoolVar(&ancestorConfig.Parse, "p", false, "Parse ancestors")
+	flag.BoolVar(&ancestorConfig.Parse, "s", false, "Save ancestors")
 	flag.Parse()
 
 }
@@ -33,9 +37,10 @@ func main() {
 		Concurrency: CONCURRENCY,
 		Once:        true,
 		// Verbose:     VERBOSE > 0,
-		Verbose:  true,
-		Store:    config.Store,
-		PageSize: 10000,
+		Verbose:        true,
+		Store:          config.Store,
+		PageSize:       10000,
+		AncestorConfig: ancestorConfig,
 	}
 
 	if err := (ingest).Exec(ctx); err != nil {
